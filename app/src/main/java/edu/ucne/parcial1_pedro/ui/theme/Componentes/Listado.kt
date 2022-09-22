74e199e30b996f74ad2b066d6cbcffefe60a5e5a
Pedro Luis Solorin <psolorin@gmail.com>
2022-09-22 18:17:23 -0400

    haciendo plantilla de listado

--- app/src/main/java/edu/ucne/parcial1_pedro/ui/theme/Componentes/Listado.kt
+++ app/src/main/java/edu/ucne/parcial1_pedro/ui/theme/Componentes/Listado.kt
@@ -1,2 +1,23 @@
 package edu.ucne.parcial1_pedro.ui.theme.Componentes
 
+import androidx.compose.material.Scaffold
+import androidx.compose.material.Text
+import androidx.compose.material.TopAppBar
+import androidx.compose.material.rememberScaffoldState
+import androidx.compose.runtime.Composable
+
+@Composable
+fun listado(){
+    val ScaffoldState = rememberScaffoldState()
+
+    Scaffold(
+        topBar = {
+            TopAppBar(title = { Text(text = "listado") })
+        },
+        scaffoldState = ScaffoldState
+
+    ){it
+
+    }
+
+}
