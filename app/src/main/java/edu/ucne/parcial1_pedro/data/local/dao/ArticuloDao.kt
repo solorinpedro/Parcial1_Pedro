23f0080b9119810e5f11a51fed7a922e55cecbd9
Pedro Luis Solorin <psolorin@gmail.com>
2022-09-28 18:22:26 -0400

    Haciendo dao de articulos

---
+++ app/src/main/java/edu/ucne/parcial1_pedro/data/local/dao/ArticuloDao.kt
@@ -0,0 +1,21 @@
+package edu.ucne.parcial1_pedro.data.local.dao
+
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import edu.ucne.parcial1_pedro.data.local.entity.Articulo
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface ArticuloDao {
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+
+    suspend fun Insertar(articulo: Articulo)
+
+    @Query(
+        "SELECT * FROM Articulos ORDER BY articuloId"
+    )
+    fun Lista():Flow<List<Articulo>>
+}
